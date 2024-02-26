dda6488352c7bc5ff129e35cf5fcc6fce968c4c7
@msfreitas <msfreitas@tre-ba.jus.br>
2024-02-26 17:50:38 -0300

    Criado laço para iterar nos dados do webservice, para encontrar as impressoras com valores iguais a zero

--- simpress_print.py
+++ simpress_print.py
@@ -4,6 +4,7 @@ import pandas as pd
 import repository_gestao_impressoras as repo
 import config
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 
@@ -42,35 +43,62 @@ def insert_printer(input_csv, brand_name):
 
     repo.cadastrarDadosImpressoras(df)
 
-def recuperar_dados_webservice(wsdl_url, service_method, payload):
-    # Create a SOAP client using the WSDL URL
-    client = zeep.Client(wsdl=wsdl_url)
+def recuperar_dados_webservice(wsdl_url, service_method, payload, timeout=5):
+    logging.info("Executando a função recuperar_dados_webservice... ")
 
-    # Access the service method and send the request with the payload
-    service = client.service
-    method_to_call = getattr(service, service_method)
-    response = method_to_call(**payload)
-    
-    # Parse the response string into Python objects
-    df = pd.read_json(response)
-    #print(df)
-    return df
+    try:
+        # Criar um cliente SOAP usando a URL do WSDL com timeout
+        client = zeep.Client(wsdl=wsdl_url, transport=zeep.Transport(timeout=timeout))
+        logging.info("Criado um cliente SOAP usando a URL do WSDL com timeout")
+
+        # Acessar o método do serviço e enviar a solicitação com os dados
+        service = client.service
+        method_to_call = getattr(service, service_method)
+        response = method_to_call(**payload)
+        logging.info("Acessar o método do serviço e enviar a solicitação com os dados")
+
+        # Analisar a string de resposta em objetos Python
+        df = pd.read_json(response)
+        return df
+    except zeep.exceptions.Fault as e:
+        logging.error(f"Erro durante a execução da função: {str(e)}")
+        # Você pode decidir levantar a exceção novamente ou retornar um valor padrão, dependendo do caso.
+        raise
+    except zeep.exceptions.TransportError as e:
+        logging.error(f"Erro de transporte durante a execução da função: {str(e)}")
+        # Trate o erro de transporte conforme necessário
+        raise
    
     # df3 = pd.merge(df_remoto, df_local, on='SERIALNUMBER', how='outer')
 def inserir_contagem_impressoras(df_remoto):
     df_remoto = df_remoto.rename(columns={"ReferenceMono" : "CONTADOR_PB", "ReferenceColor" : "CONTADOR_COR", "DateTimeRead" : "DATA_LEITURA"})
     df_remoto.columns = df_remoto.columns.str.upper()
-    df_remoto = df_remoto[['SERIALNUMBER', 'DATA_LEITURA', 'CONTADOR_PB', 'CONTADOR_COR']]
     df_remoto['CONTADOR_TOTAL'] = df_remoto['CONTADOR_PB'] + df_remoto['CONTADOR_COR']
-    # print(df_remoto)
-    df_local = repo.recuperarDadosLocaisImpressoras()
-    df_remoto = df_remoto.rename(columns={"ReferenceMono" : "CONTADOR_PB", "ReferenceColor" : "CONTADOR_COR", "DateTimeRead" : "DATA_LEITURA"})
+    # df_remoto = df_remoto[['SERIALNUMBER', 'DATA_LEITURA', 'CONTADOR_PB', 'CONTADOR_COR']]
+    # df_remoto = df_remoto.rename(columns={"ReferenceMono" : "CONTADOR_PB", "ReferenceColor" : "CONTADOR_COR", "DateTimeRead" : "DATA_LEITURA"})
+    # df_local = repo.recuperarDadosLocaisImpressoras()
        
-    print("Dados do banco local")
-    print(df_local)
+    # print("Dados do banco local")
+    # print(df_local)
     print()
-    print("Dados do banco remoto")
+    # print("Dados do banco remoto")
     print(df_remoto)
+    # df_remoto.to_csv("dataframe_remoto.csv", sep=';')
+    # df_zeros = df_remoto.loc[df_remoto['CONTADOR_TOTAL'] == 0]
+    # print(df_zeros)
+
+  
+    # TODO criar laço para iterar no dataframe com valores iguais a zero.
+    df_remoto.sort_values(by='DATA_LEITURA', ascending=True, inplace=True)
+    for index, row in df_remoto.iterrows():
+        if row['CONTADOR_TOTAL'] == 0:
+            print(index, row['PRINTERDEVICEID'],row['DATA_LEITURA'], row['CONTADOR_TOTAL'])
+    
+    # TODO buscar nos dados do webservice (dia anterior) os valores não nulos para cada impressora encontrada no passo anterior
+    # TODO se houver valores no dia anterior, preencher os dados do banco com os valores não nulos recuperados
+    # TODO se não houver valor na data anterior, procurar as outras datas até achar uma com valores não nulos
+    # TODO 
+
 
 # Dados api
 wsdl_url = config.wsdl_url
@@ -80,7 +108,7 @@ payload = config.payload
 
 #insert_printer('output-reference-2023-10-17.csv', 'HP')
 #send_soap_request_and_write_to_csv(wsdl_url, service_method, output_csv, payload)
-df_remoto = recuperar_dados_webservice(wsdl_url, service_method, payload)
+df_remoto = recuperar_dados_webservice(wsdl_url, service_method, payload,timeout=5)
 
 # dfContagem = inserir_contagem_impressoras(dfContagem)
 df_local = repo.recuperarDadosLocaisImpressoras()
