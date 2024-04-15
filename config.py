074099f3146fd377e430073e6304281ec7a75ce4
Marcio Freitas <msfreitas@tre-ba.jus.br>
2024-04-15 16:43:40 -0300

    Atribuída a variável 'dateTimeEnd' uma valor que captura a data atual e a trasforma em uma string

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 wsdl_url = 'https://api-counters.nddprint.com/CountersWS/CountersData.asmx?WSDL'
 service_method = 'GetReferenceCountersData' #'GetPlainCountersData'
 output_csv = 'output-reference-2023-10-27.csv' #'output-plain-2023-10-15.csv'
@@ -6,7 +9,8 @@ payload = {
     'dealerUserEmail': 'ruguedes@tre-ba.jus.br',
     'dealerUserPassword': '8lYKAfLbl2FKqAJgWWRA5Q==',
     # 'dateTimeStart': '2023-01-01 00:00:00',
-    'dateTimeEnd': '2024-04-11 02:00:00',
+    # 'dateTimeEnd': '2024-04-11 02:00:00',
+    'dateTimeEnd': datetime.now().strftime('%Y-%m-%d %H%M%S'),
     'maxLimitDaysEarlier': 1,
     'enterpriseName': '9853_TRE_BA',
     'serialNumber': '',
