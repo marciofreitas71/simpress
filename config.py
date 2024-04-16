af0a92395a32ce014cc9d533f6ba274765e04526
Marcio Freitas <msfreitas@tre-ba.jus.br>
2024-04-16 18:13:41 -0300

    Criação de variável 'dateTimeEnd(data final de aferição do payload)

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+import simpress_print
 
+dateTimeEnd = f"{datetime.now().strftime('%Y-%m-%d')} 02:00:00"
 
 wsdl_url = 'https://api-counters.nddprint.com/CountersWS/CountersData.asmx?WSDL'
 service_method = 'GetReferenceCountersData' #'GetPlainCountersData'
@@ -10,7 +12,7 @@ payload = {
     'dealerUserPassword': '8lYKAfLbl2FKqAJgWWRA5Q==',
     # 'dateTimeStart': '2023-01-01 00:00:00',
     # 'dateTimeEnd': '2024-04-11 02:00:00',
-    'dateTimeEnd': f"{datetime.now().strftime('%Y-%m-%d')} 02:00:00" ,
+    'dateTimeEnd': dateTimeEnd ,
     'maxLimitDaysEarlier': 1,
     'enterpriseName': '9853_TRE_BA',
     'serialNumber': '',
