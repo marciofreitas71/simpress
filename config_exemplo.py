4029d646d1f9c0c818f841e5fdbb18f98a1ca72d
Marcio Freitas <msfreitas@tre-ba.jus.br>
2024-04-17 08:53:48 -0300

    Atribuição de string vazia à chave 'dateTimeEnd'

--- config_exemplo.py
+++ config_exemplo.py
@@ -6,7 +6,7 @@ payload = {
     'dealerUserEmail': 'xxxxx@xxx.com.br',
     'dealerUserPassword': 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     #'dateTimeStart': '2023-10-17 00:00:00',
-    'dateTimeEnd': '2023-11-30 02:00:00',
+    'dateTimeEnd': '',
     'maxLimitDaysEarlier': 1,
     'enterpriseName': '9853_TRE_BA',
     'serialNumber': '',
