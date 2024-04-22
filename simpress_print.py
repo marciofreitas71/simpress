2292bda3c5e7b519f80815863f88d635e8d11f47
Marcio Freitas <msfreitas@tre-ba.jus.br>
2024-04-22 13:16:29 -0300

    Acrescentada à função transforma_df_remoto() a lógica para busca de valores não zerados no banco de dados

--- simpress_print.py
+++ simpress_print.py
@@ -91,33 +91,78 @@ def transforma_df_remoto():
     # Chame a função recuperar_dados_webservice() com dateTimeEnd como argumento adicional
     df_remoto = recuperar_dados_webservice(wsdl_url, service_method, payload, dateTimeEnd, timeout=timeout)
 
-    # Cria um dataframe contendo as impressoras com valores iguais a 0
-    impressoras_com_zero = df_remoto[(df_remoto['ReferenceMono'] == 0) | (df_remoto['ReferenceColor'] == 0)]      
-    df_remoto.to_csv(f"testes/df_remoto-{dateTimeEnd[:11]}.csv")
-    
-    # Converta dateTimeEnd em um objeto datetime
+    # Armazena a data atual
     data_atual = datetime.strptime(dateTimeEnd, "%Y-%m-%d %H:%M:%S")
 
-    # Armazenar a data do dia anterior
-    days = 3
-    data_anterior = data_atual - timedelta(days=days)
-    print(data_anterior)
+    # Inicializa df_remoto_2 como None
+    df_remoto_2 = None
 
+    # Armazena a data do dia anterior
+    data_anterior = data_atual - timedelta(days=1)
+
+    # Continua buscando em dias anteriores até que os valores sejam preenchidos
+    while df_remoto_2 is None or df_remoto_2[(df_remoto_2['ReferenceMono'] == 0) | (df_remoto_2['ReferenceColor'] == 0)].any(axis=None):        
+        print(f'----------------------------------------------------')
+        print(f'Pesquisando em {data_anterior}...')
+        print(f'----------------------------------------------------')
+        
+        # Verifica se df_remoto_2 já foi buscado, se não, busca
+        df_remoto_2 = recuperar_dados_webservice(wsdl_url, service_method, payload, data_anterior.strftime("%Y-%m-%d %H:%M:%S"), timeout=timeout)
+
+        # Substituição dos valores no dataframe
+        for index, row in df_remoto[(df_remoto['ReferenceMono'] == 0) | (df_remoto['ReferenceColor'] == 0)].iterrows():
+            PrinterDeviceID = row['PrinterDeviceID']
+            ReferenceMono = row['ReferenceMono']
+            ReferenceColor = row['ReferenceColor']
+  
+            # Verifica se o valor de ReferenceMono é igual a 0
+            if ReferenceMono == 0:
+                # Busca a impressora no df_remoto_2
+                printer_data = df_remoto_2[df_remoto_2['PrinterDeviceID'] == PrinterDeviceID]
+                if not printer_data.empty:
+                    # Obtém o valor de ReferenceMono do registro correspondente no dia anterior
+                    ReferenceMono_anterior = printer_data.iloc[0]['ReferenceMono']
+                    # Se o valor for diferente de 0, substitui
+                    if ReferenceMono_anterior != 0:
+                        df_remoto.at[index, 'ReferenceMono'] = ReferenceMono_anterior
+                        print("O valor da impressão em preto e branco foi substituido")
+                        print(f'Valor anterior: {PrinterDeviceID}')
+                        print(f'Valor atual: {ReferenceMono_anterior}')
+
+            # Verifica se o valor de ReferenceColor é igual a 0
+            if ReferenceColor == 0:
+                # Busca a impressora no df_remoto_2
+                printer_data = df_remoto_2[df_remoto_2['PrinterDeviceID'] == PrinterDeviceID]
+                if not printer_data.empty:
+                    # Obtém o valor de ReferenceColor do registro correspondente no dia anterior
+                    ReferenceColor_anterior = printer_data.iloc[0]['ReferenceColor']
+                    # Se o valor for diferente de 0, substitui
+                    if ReferenceColor_anterior != 0:
+                        df_remoto.at[index, 'ReferenceColor'] = ReferenceColor_anterior                        
+                        print(f'Valor anterior: {PrinterDeviceID}')
+                        print(f'Valor atual: {ReferenceColor_anterior}')
+
+        # Atualiza a data atual para o dia anterior se os valores ainda estiverem vazios
+        if df_remoto[(df_remoto['ReferenceMono'] == 0) | (df_remoto['ReferenceColor'] == 0)].any(axis=None):
+            data_anterior -= timedelta(days=1)
+            print(data_anterior)
+                        
+        #     # Verifica se há valores zero em 'ReferenceMono' ou 'ReferenceColor'
+        #     registros_zerados = df_remoto[(df_remoto['ReferenceMono'] == 0) | (df_remoto['ReferenceColor'] == 0)].shape[0]
+            
+        #     # Imprime o número de registros com valores zerados
+        #     print("Número de registros com valores zerados:", registros_zerados)
+
+            
+
+    
+        
 
     # # Converter a data anterior para o formato desejado
     # data_anterior_formatada = data_anterior.strftime('%Y-%m-%d 02:00:00')
 
     # print(data_anterior_formatada)
 
-    # Carrega dados do dia anterior
-
-
-    
-    # Recuperar os dados do dia anterior
-
-    # 
-    # Se a impressora 
-    # Vefificar se a impressora de mesmo id tem valores diferentes de zero
      
 
 if __name__ == "__main__":
