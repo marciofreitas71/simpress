35954b6739184431404758296da10893f8297135
Marcio Freitas <msfreitas@tre-ba.jus.br>
2024-04-26 11:25:02 -0300

    Inclusão de função para obter o nome do host a partir do IP

--- popula_banco.py
+++ popula_banco.py
@@ -1,6 +1,16 @@
 from datetime import datetime, timedelta
 import repository_gestao_impressoras as repo
 
+def obter_hostname_por_ip(ip):
+    try:
+        hostname = socket.gethostbyaddr(ip[:-10])
+        return hostname[0]  # O nome do host está na primeira posição da tupla retornada
+    except socket.herror:
+        return "Hostname não encontrado"
+    except socket.gaierror:
+        return "Endereço IP inválido"
+
+
 def salva_dados_csv():
     DateTimeEnd = datetime.strptime('2024-04-25', '%Y-%m-%d')
 
@@ -13,6 +23,8 @@ def salva_dados_csv():
         
         # Adiciona a nova coluna com a DateTimeEnd
         dados_webservice['RealDataCapture'] = DateTimeEnd.strftime('%Y-%m-%d')
+
+        # 
         
         # Salva os dados em um arquivo CSV
         dados_webservice.to_csv(f'arquivos/dados-{DateTimeEnd.strftime("%Y-%m-%d")}.csv', index=False)
