0abcb43837531f726dd0b12aa44b1f79a79734b0
Marcio Freitas <msfreitas@tre-ba.jus.br>
2024-04-29 14:58:16 -0300

    Correção da conexão com o banco de dados

--- repository_gestao_impressoras.py
+++ repository_gestao_impressoras.py
@@ -1,7 +1,9 @@
 import os
 from sqlalchemy import create_engine
 import pandas as pd
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def getConnection():
     # Conecta ao banco de dados Oracle
@@ -10,7 +12,7 @@ def getConnection():
     host = os.getenv('host')
     port = os.getenv('port')
     service_name = os.getenv('service_name')
-    dsn = f"oracle+cx_oracle://{user_name}:{password}@{host}:{port}/{service_name}"
+    dsn = f"oracle+oracledb://{user_name}:{password}@{host}:{port}/{service_name}"
     return create_engine(dsn)
 
 # Função para executar a consulta
@@ -18,7 +20,7 @@ def query_impressoras():
     engine = getConnection()
     
     # Consulta SQL para selecionar todas as colunas da tabela 'impressoras'
-    query = "SELECT * FROM impressoras"
+    query = "SELECT * FROM IMPRESSORA"
     
     # Executa a consulta e carrega os resultados em um DataFrame
     df = pd.read_sql(query, engine)
