d61847b264cd3228be24ea87fbf36122bffbf11d
Marcio Freitas <msfreitas@tre-ba.jus.br>
2024-04-29 19:16:17 -0300

    corrigido carregamento das variáveis no .env

--- consulta_banco.py
+++ consulta_banco.py
@@ -1,6 +1,10 @@
 import os
 from sqlalchemy import create_engine
 import pandas as pd
+from dotenv import load_dotenv
+import repository_gestao_impressoras as repo
+
+load_dotenv()
 
 def getConnection():
     # Conecta ao banco de dados Oracle
@@ -15,28 +19,31 @@ def getConnection():
         print("Erro: Algum valor necessário está ausente.")
         return None
     
-    dsn = f"oracle+cx_oracle://{user_name}:{password}@{host}:{port}/{service_name}"
+    dsn = f"oracle+oracledb://{user_name}:{password}@{host}:{port}/{service_name}"
     return create_engine(dsn)
 
-# Função para executar a consulta
-def query_impressoras():
-    engine = getConnection()
+# # Função para executar a consulta
+# def query_impressoras():
+#     engine = getConnection()
     
-    # Verifica se a conexão foi estabelecida com sucesso
-    if engine is None:
-        print("Erro: Não foi possível estabelecer conexão com o banco de dados.")
-        return None
+#     # Verifica se a conexão foi estabelecida com sucesso
+#     if engine is None:
+#         print("Erro: Não foi possível estabelecer conexão com o banco de dados.")
+#         return None
     
-    # Consulta SQL para selecionar todas as colunas da tabela 'impressoras'
-    query = "SELECT * FROM impressoras"
+#     # Consulta SQL para selecionar todas as colunas da tabela 'impressoras'
+#     query = "SELECT * FROM impressora"
     
-    # Executa a consulta e carrega os resultados em um DataFrame
-    df = pd.read_sql(query, engine)
+#     # Executa a consulta e carrega os resultados em um DataFrame
+#     df = pd.read_sql(query, engine)
     
-    # Retorna o DataFrame com os resultados da consulta
-    return df
+#     # Retorna o DataFrame com os resultados da consulta
+#     return df
+
+# Executa a consulta e imprime os resultados
 
 
 # Executa a consulta e imprime os resultados
-impressoras_df = query_impressoras()
-print(impressoras_df)
+impressoras = repo.query_impressoras()
+for impressora in impressoras:
+    print(impressora)
