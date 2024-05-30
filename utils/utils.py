645925961d74c019e9c0dacb25def417837e0f76
Marcio Freitas <msfreitas@tre-ba.jus.br>
2024-05-29 21:59:04 -0300

    Criação da função def insere_websersvice_banco(data)

--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,85 @@
-from app import config, webservice
+from app import config, webservice, crud
 import logging
-from app import crud
+from tqdm import tqdm
 from datetime import datetime, timedelta
+import itertools
 import pandas as pd
+import numpy as np
 import glob
 import csv
 import os
+
+
+def preenche_dados_csv(caminho_csv):
+    # Carregar o conjunto de dados
+    print("Carregando o conjunto de dados...")
+    df = pd.read_csv(caminho_csv)
+    df = df[df['BrandName'] == 'HP']
+    df = df.sort_values(by=['RealDateCapture','SerialNumber'], ascending=[False,True])
+
+    # Converte a coluna 'RealDateCapture' do df para datetime
+    df['RealDateCapture'] = pd.to_datetime(df['RealDateCapture'], format='%d-%m-%Y')
+
+    # Salva o arquivo csv com os dados originais
+    df.to_csv('df_original.csv')
+    print(f"Conjunto de dados carregado com {len(df)} registros.")
+
+  # Selecionar impressoras únicas (linhas únicas com base na coluna 'SerialNumber')
+    imp = df.drop_duplicates(subset='SerialNumber')
+
+    # Seleciona as colunas do DataFrame 'imp'
+    imp = imp[['EnterpriseName', 'PrinterDeviceID', 'BrandName', 'PrinterModelName', 'SerialNumber']]
+
+    # Gera arquivo csv com as impressoras do dataframe
+    imp.to_csv('testes/data/df_impressoras.csv')
+
+    # Gera um DataFrame com datas
+    date_range = pd.date_range(start='01-01-2022', end='28-05-2024', freq='D')
+    df_dates = pd.DataFrame({'RealDateCapture': date_range})
+
+    # Adicionar a coluna 'key' para combinação
+    imp['key'] = 1
+    df_dates['key'] = 1
+
+    # Combinação dos DataFrames
+    df_full_datas = pd.merge(imp, df_dates, on='key').drop('key', axis=1)
+
+    # Gera um arquivo csv com as datas mescladas com os dados
+    df_full_datas.to_csv("df_full_datas.csv")
+
+    # Faz o merge com o DataFrame original
+    df_merged = df_full_datas.merge(df, how='left', on=['RealDateCapture','SerialNumber','EnterpriseName','PrinterDeviceID','BrandName','PrinterModelName'])
+
+    # Preenche os valores ausentes com nan
+    df_merged.fillna(value=np.nan, inplace=True)
+
+    # ordena os registros pelas colunas 'RealDateCapture' e 'SerialNumber'
+    df_merged = df_merged.sort_values(by=['RealDateCapture','SerialNumber'], ascending=False)
+
+    # Gera um arquivo csv com os dados mescladas
+    df_merged.to_csv('df_merged.csv')
+
+    # Inicializa uma lista vazia para armazenar DataFrames individuais correspondentes a cada número de série
+    registros_impressoras = []
+    # Itera sobre cada linha do DataFrame 'imp'
+    for index, row in imp.iterrows():
+        serial_number = row['SerialNumber']
+        # Filtra o DataFrame 'df_merged' para obter apenas as linhas com o mesmo número de série e preenche valores nulos usando o método backward fill (bfill)
+        df_parcial = df_merged[df_merged['SerialNumber'] == serial_number].bfill()
+        # Adiciona o DataFrame filtrado e preenchido à lista 'impressoras'
+        registros_impressoras.append(df_parcial)
+
+    # Concatena todos os DataFrames na lista 'impressoras' em um único DataFrame, ignorando os índices originais
+    df_filled = pd.concat(registros_impressoras, ignore_index=True)
+
+    # Converte a coluna 'RealDateCapture' para o tipo datetime
+    df_filled['RealDateCapture'] = pd.to_datetime(df_filled['RealDateCapture'])
+    # Filtra o DataFrame para incluir apenas os registros onde 'RealDateCapture' é posterior à data de 01/01/2024
+    df_filled_2024 = df_filled[df_filled['RealDateCapture'] >= '2024-01-01']
+    # Preenche os valores nulos do DataFrame filtrado com 0
+    df_filled_2024 = df_filled_2024.fillna(0)
+    # Salvar o DataFrame resultante
+    df_filled_2024.to_csv('D:/projetos/simpress/testes/df_filled_2024.csv', index=False)
 
 def insere_dados_csv_to_bd():
     """
@@ -231,26 +305,47 @@ def extrair_id_zona(ip):
 
 
 def insere_websersvice_banco(data):
+    """
+    Insere os dados do webservice no banco de dados após verificar e comparar com os dados existentes.
+    """
+    # Recuperar a data do dia anterior
+    data_bd = datetime.strptime(data, '%d-%m-%Y') - timedelta(days=1)
+    data_bd_str = data_bd.strftime('%d-%m-%Y')
 
-    # Data para recuperação dos dados
-    data = datetime.now()
-
-    # criar um dataframe 'df_webservice'
-    df_webservice = recuperar_dados(data)
+    # Recuperar dados do webservice
+    df_webservice = webservice.recuperar_dados(data)
     
-    # 'DateTimeRead' para datetime
+    # Converter 'DateTimeRead' para datetime
     df_webservice['DateTimeRead'] = pd.to_datetime(df_webservice['DateTimeRead'])
 
-    # fazer uma consulta no banco de dados (Data do webservice - 1 dia)
-    data_bd = data - 1
+    # Obter dados do banco de dados para a data do dia anterior
+    registros_bd = crud.read_impressoras_data(data_bd_str)
 
-    # Criar um dataframe 'df_database'
-    
-    # comparar os dois datasets para saber quais impressoras do dataframe 'df_database'
-    # estão no dataframe 'df_webservice'
-    # carregar um dataframe com a diferenca entre os dois 'df_diff' 
-    # criar um dataframe 'df_final' concatenando o data 'df_diff' ao 'df_webservice'
-    # inserir no banco de dados os dados do dataframe 'df_final'
+    # Criar um DataFrame com os dados do banco de dados
+    if registros_bd:
+        colunas = ['IMPRESSORA_ID', 'CONTADOR_PB', 'CONTADOR_COR', 'CONTADOR_TOTAL', 'DATA_LEITURA', 'CREATED_AT']
+        df_database = pd.DataFrame(registros_bd, columns=colunas)
+    else:
+        df_database = pd.DataFrame(columns=['IMPRESSORA_ID', 'CONTADOR_PB', 'CONTADOR_COR', 'CONTADOR_TOTAL', 'DATA_LEITURA', 'CREATED_AT'])
+
+    # Comparar os dois DataFrames para saber quais impressoras do df_database estão no df_webservice
+    df_diff = pd.concat([df_webservice, df_database]).drop_duplicates(subset=['IMPRESSORA_ID', 'DateTimeRead'], keep=False)
+
+    # Criar um DataFrame final concatenando o df_diff ao df_webservice
+    df_final = pd.concat([df_webservice, df_diff])
+
+    # Inserir no banco de dados os dados do DataFrame final
+    for index, row in df_final.iterrows():
+        try:
+            crud.create_contagem_impressoras(
+                row['IMPRESSORA_ID'],
+                row['CONTADOR_PB'],
+                row['CONTADOR_COR'],
+                row['DateTimeRead']
+            )
+            print(f"Registro ({row['IMPRESSORA_ID']}) inserido com sucesso.")
+        except Exception as e:
+            print(f"Erro ao inserir registro de contagem para o registro com IMPRESSORA_ID {row['IMPRESSORA_ID']}: {e}")
 
     
 def gera_arquivo_csv_compilado(pasta):
