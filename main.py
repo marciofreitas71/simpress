e98ef951d8a3512d179faa8f51b987e62bd60e37
Marcio Freitas <msfreitas@tre-ba.jus.br>
2024-05-31 17:19:23 -0300

    inserção da função insere_webservice no arquivo main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     # utils.preenche_dados_csv('D:/projetos/simpress/testes/arquivo_final-29-05-2024.csv')
 
     # Insere os dados no dataframe
-    utils.insere_dados_csv_to_bd()
+    # utils.insere_dados_csv_to_bd()
 
     # # Atualização da lista de impressoras
     # df = pd.read_csv('testes/data/df_impressoras.csv')
@@ -26,7 +26,7 @@ def main():
     #     PRINTERMODELNAME = row['PrinterModelName']
     #     SERIALNUMBER = row['SerialNumber']
     #     crud.create_impressora(PRINTERDEVICEID, PRINTERBRANDNAME, PRINTERMODELNAME, SERIALNUMBER)
-        
+    utils.insere_websersvice_banco('29-05-24')
 
 # A seguinte linha é executada somente se o módulo main.py for executado diretamente
 if __name__ == "__main__":
