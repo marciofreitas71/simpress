3f9f60d8a7fda51e842c446f8295ae0c26ba822c
Marcio Freitas <marciofreitas71@yahoo.com>
2024-05-31 21:23:09 -0300

    Exclusão das funções para que fossem transferidas para os pacotes scripts

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
 from app import webservice, crud
 from utils import utils
-import os
 import logging
-import itertools
-import pandas as pd
-import numpy as np
-import zeep
+
 
 def main():
-
+    pass
           
-    # utils.salva_dados_csv('01-01-2022', '28-05-2024')
-    # utils.gera_arquivo_csv_compilado('D:/projetos/simpress/testes/arquivos_final')
-    
-    # utils.preenche_dados_csv('D:/projetos/simpress/testes/arquivo_final-29-05-2024.csv')
 
-    # Insere os dados no dataframe
-    # utils.insere_dados_csv_to_bd()
-
-    # # Atualização da lista de impressoras
-    # df = pd.read_csv('testes/data/df_impressoras.csv')
-    # for index, row in df.iterrows():
-    #     PRINTERDEVICEID = row['PrinterDeviceID']
-    #     PRINTERBRANDNAME = row['BrandName']
-    #     PRINTERMODELNAME = row['PrinterModelName']
-    #     SERIALNUMBER = row['SerialNumber']
-    #     crud.create_impressora(PRINTERDEVICEID, PRINTERBRANDNAME, PRINTERMODELNAME, SERIALNUMBER)
-    utils.insere_websersvice_banco('29-05-24')
-
-# A seguinte linha é executada somente se o módulo main.py for executado diretamente
 if __name__ == "__main__":
     main()
