30451483ffb7b4de7f7a8c4a9cb9eabf270b040e
Marcio Freitas <marciofreitas71@yahoo.com>
2024-06-03 11:04:28 -0300

    modificado :scripts/exclue_dados.py
    import os
    import shutil
    
    def excluir_arquivos_recursivamente(pasta):
        """
        Exclui todos os arquivos dentro de uma pasta de forma recursiva.
    
        Args:
            pasta (str): O caminho da pasta onde os arquivos serão excluídos.
        """
        for root, dirs, files in os.walk(pasta):
            for file in files:
                file_path = os.path.join(root, file)
                try:
                    os.remove(file_path)
                    print(f'{file_path} excluído com sucesso.')
                except Exception as e:
                    print(f'Erro ao excluir {file_path}: {e}')
    
    if __name__ == "__main__":
        # Executa a função para excluir arquivos na pasta 'temp' recursivamente
        excluir_arquivos_recursivamente('temp')

--- scripts/exclue_dados.py
+++ scripts/exclue_dados.py
@@ -2,6 +2,12 @@ import os
 import shutil
 
 def excluir_arquivos_recursivamente(pasta):
+    """
+    Exclui todos os arquivos dentro de uma pasta de forma recursiva.
+
+    Args:
+        pasta (str): O caminho da pasta onde os arquivos serão excluídos.
+    """
     for root, dirs, files in os.walk(pasta):
         for file in files:
             file_path = os.path.join(root, file)
@@ -13,4 +19,5 @@ def excluir_arquivos_recursivamente(pasta):
             
 
 if __name__ == "__main__":
+    # Executa a função para excluir arquivos na pasta 'temp' recursivamente
     excluir_arquivos_recursivamente('temp')
