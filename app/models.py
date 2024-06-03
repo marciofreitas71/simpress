863275357247e2c2bcf6658d681870a0838dea5a
Marcio Freitas <marciofreitas71@yahoo.com>
2024-06-03 08:22:50 -0300

    Modificado:   app/models.py
    Melhorias na documentação das classes de modelo de dados
    
    - Adicionadas anotações de tipo aos parâmetros do método __init__ das classes ContagemImpressoras e Impressoras.
    - Melhorada a estrutura das docstrings para maior clareza e consistência.
    - Incluídas descrições detalhadas dos parâmetros do método __init__ para ambas as classes.
    - Mantida a documentação clara e informativa sobre os atributos e a finalidade das classes.

--- app/models.py
+++ app/models.py
@@ -19,7 +19,19 @@ class ContagemImpressoras:
         DATA_LEITURA (datetime): A data e hora da leitura do contador.
         CREATED_AT (datetime): A data e hora de criação do registro.
     """
-    def __init__(self, ID, IMPRESSORA_ID, CONTADOR_PB, CONTADOR_COR, CONTADOR_TOTAL, DATA_LEITURA, CREATED_AT):
+    def __init__(self, ID: int, IMPRESSORA_ID: int, CONTADOR_PB: int, CONTADOR_COR: int, CONTADOR_TOTAL: int, DATA_LEITURA, CREATED_AT):
+        """
+        Inicializa uma nova instância da classe ContagemImpressoras.
+
+        Args:
+            ID (int): O ID da contagem.
+            IMPRESSORA_ID (int): O ID da impressora associada à contagem.
+            CONTADOR_PB (int): O contador de impressões em preto e branco.
+            CONTADOR_COR (int): O contador de impressões coloridas.
+            CONTADOR_TOTAL (int): O contador total de impressões.
+            DATA_LEITURA (datetime): A data e hora da leitura do contador.
+            CREATED_AT (datetime): A data e hora de criação do registro.
+        """
         self.ID = ID
         self.IMPRESSORA_ID = IMPRESSORA_ID
         self.CONTADOR_PB = CONTADOR_PB
@@ -41,7 +53,19 @@ class Impressoras:
         CREATED_AT (datetime): A data e hora de criação do registro.
         STATUS (int): O status da impressora.
     """
-    def __init__(self, ID, PRINTERDEVICEID, PRINTERBRANDNAME, PRINTERMODELNAME, SERIALNUMBER, CREATED_AT, STATUS):
+    def __init__(self, ID: int, PRINTERDEVICEID: str, PRINTERBRANDNAME: str, PRINTERMODELNAME: str, SERIALNUMBER: str, CREATED_AT, STATUS: int):
+        """
+        Inicializa uma nova instância da classe Impressoras.
+
+        Args:
+            ID (int): O ID da impressora.
+            PRINTERDEVICEID (str): O ID do dispositivo da impressora.
+            PRINTERBRANDNAME (str): O nome da marca da impressora.
+            PRINTERMODELNAME (str): O nome do modelo da impressora.
+            SERIALNUMBER (str): O número de série da impressora.
+            CREATED_AT (datetime): A data e hora de criação do registro.
+            STATUS (int): O status da impressora.
+        """
         self.ID = ID
         self.PRINTERDEVICEID = PRINTERDEVICEID
         self.PRINTERBRANDNAME = PRINTERBRANDNAME
