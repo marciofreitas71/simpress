3f7637286a1c16432564918fbb3f280c4b180286
Marcio Freitas <marciofreitas71@yahoo.com>
2024-06-03 07:53:44 -0300

    Implementação do agendamento de tarefas com APScheduler e logging
    - Adicionado o uso do APScheduler para agendar a execução da função insere_websersvice_bd() diariamente às 3:00:00.
    - Configurado o logging para registrar a execução do scheduler e da função insere_websersvice_bd() em um arquivo de log.
    - Implementada a função insere_websersvice_bd() que chama a função insere_websersvice_banco do módulo utils e registra a execução e conclusão no log.

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 from app import webservice, crud
 from utils import utils
-import logging
+import logging   
+from apscheduler.schedulers.blocking import BlockingScheduler
+import os
 
-
-def main():
-    pass
-          
+def insere_websersvice_bd():
+    """
+    Chama a função insere_websersvice_banco do módulo utils e faz o logging.
+    
+    Esta função é responsável por inserir dados do webservice no banco de dados.
+    A execução e a conclusão da função são registradas no log.
+    """
+    logging.info("Executando insere_websersvice_banco")
+    utils.insere_websersvice_banco()
+    logging.info("Execução de insere_websersvice_banco concluída")
 
 if __name__ == "__main__":
-    main()
+    # Cria a pasta logs se não existir
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+
+    # Configuração do logging
+    logging.basicConfig(filename='logs/scheduler.log', level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info('Scheduler iniciado')
+
+    # Configuração do agendamento
+    scheduler = BlockingScheduler()
+    # Adiciona um trabalho ao scheduler para executar insere_websersvice_bd diariamente às 3:00:00
+    scheduler.add_job(insere_websersvice_bd, 'cron', hour=3, minute=0, second=0)
+
+    try:
+        logging.info('Scheduler em execução')
+        # Inicia o scheduler para começar a execução das tarefas agendadas
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        logging.info('Scheduler interrompido')
+        # Encerra o scheduler de forma segura
+        scheduler.shutdown()
