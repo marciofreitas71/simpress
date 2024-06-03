ac647edde2423aebf4b1c60e7e4beb0e9c5cbe45
Marcio Freitas <marciofreitas71@yahoo.com>
2024-06-03 10:57:11 -0300

    modificado: app/webservice.py
    Melhorias na documentação do módulo webservice
    
    - Adicionadas descrições detalhadas sobre a função recuperar_dados para incluir informações sobre o processamento de dados.
    - Adicionado exemplo de uso na docstring da função recuperar_dados.
    - Melhorados os comentários no código para maior clareza e consistência.
    - Mantida a estrutura geral e clareza da documentação do módulo e da função.

--- app/webservice.py
+++ app/webservice.py
@@ -44,26 +44,35 @@ def recuperar_dados(data):
     Raises:
         zeep.exceptions.Fault: Se ocorrer um erro durante a execução do serviço.
         zeep.exceptions.TransportError: Se ocorrer um erro de transporte durante a execução do serviço.
+    
+    Exemplo:
+        >>> df = recuperar_dados("01-01-2023")
     """
     logging.info("Executando a função recuperar_dados_webservice... ")
+    
+    # Converte a data fornecida para um objeto datetime
     data_objeto = datetime.strptime(data, "%d-%m-%Y")
     data_objeto += timedelta(days=1)
     dateTimeEnd = data_objeto.strftime("%Y-%m-%d")
     config.payload['dateTimeEnd'] = f'{dateTimeEnd} 00:00:00'
 
     try:
+        # Cria um cliente SOAP usando a URL do WSDL com timeout
         client = zeep.Client(wsdl=config.wsdl_url, transport=zeep.Transport(timeout=10))
-        logging.info("Criado um cliente SOAP usando a URL do WSDL com timeout")
+        logging.info("Cliente SOAP criado com sucesso")
+        
+        # Acessa o método do serviço e envia a solicitação com os dados
         service = client.service
         method_to_call = getattr(service, config.service_method)
         response = method_to_call(**config.payload)
-        logging.info("Acessar o método do serviço e enviar a solicitação com os dados")
+        logging.info("Solicitação enviada ao serviço SOAP")
 
+        # Processa a resposta do serviço e converte para DataFrame
         df = pd.read_json(StringIO(response))
         return df
     except zeep.exceptions.Fault as e:
-        logging.error(f"Erro durante a execução da função: {str(e)}")
+        logging.error(f"Erro durante a execução do serviço: {str(e)}")
         raise
     except zeep.exceptions.TransportError as e:
-        logging.error(f"Erro de transporte durante a execução da função: {str(e)}")
+        logging.error(f"Erro de transporte durante a execução do serviço: {str(e)}")
         raise
