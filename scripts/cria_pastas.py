c81a1f7baf14af85b6ecdce83ea0d9b921d49ea4
Marcio Freitas <marciofreitas71@yahoo.com>
2024-06-03 11:02:39 -0300

    scripts/cria_pastas.py Melhorias na documentação do script de criação de diretórios
    - Adicionada docstring para a função criar_pastas_projeto explicando seu propósito e listando os diretórios que serão criados.
    - Adicionado comentário no bloco principal para explicar a execução da função quando o script é executado diretamente.

--- scripts/cria_pastas.py
+++ scripts/cria_pastas.py
@@ -1,6 +1,13 @@
 import os
 
 def criar_pastas_projeto():
+    """
+    Cria os diretórios necessários para o projeto, se eles não existirem.
+
+    Os diretórios a serem criados são:
+    - 'temp/dados_compilados'
+    - 'temp/dados_diarios'
+    """
     diretorios = [
         'temp/dados_compilados',
         'temp/dados_diarios'
@@ -11,4 +18,5 @@ def criar_pastas_projeto():
         print(f'Diretório "{diretorio}" criado ou já existe.')
 
 if __name__ == "__main__":
+    # Executa a função para criar os diretórios do projeto
     criar_pastas_projeto()
